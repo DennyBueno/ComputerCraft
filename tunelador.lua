0dc19c79ab76cdbaa0bb8e3c1c90ab23785c49ee
Rafael de Paiva Rogerio <rrogerio@exactpay.com>
2023-08-07 22:01:33 -0300

    Making the code DRY

--- tunelador.lua
+++ tunelador.lua
@@ -1,32 +1,24 @@
 total=20
 
-function
-
-for num=1,tonumber(total) do
-  print("o valor de num e ".. tostring(num))
-
+function faztunel()
   turtle.dig()
   turtle.forward()
   turtle.digUp()
   turtle.digDown()
+end
 
+for num=1,tonumber(total) do
+  print("o valor de num e ".. tostring(num))
+  faztunel()
 end
 --vire pra direta
 turtle.turnRight()
 for num2=1,3 do
-  turtle.dig()
-  turtle.forward()
-  turtle.digUp()
-  turtle.digDown()
-  
+  faztunel()
 end
 
 turtle.turnRight()
 for num3=1,tonumber(total) do
-  turtle.dig()
-  turtle.forward()
-  turtle.digUp()
-  turtle.digDown()
-  
-end    
+  faztunel()
+end
 
