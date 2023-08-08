d3854b6d2bab0bd7a28908449f139f53a60ca18d
DennyBueno <debueno.93@gmail.com>
2023-08-07 22:37:36 -0300

    Sistema de iluminação

--- tunelador.lua
+++ tunelador.lua
@@ -1,4 +1,12 @@
 total=20
+contador=1
+
+function botaTocha()
+  --Colocar tochas no slot 15
+  turtle.select(15)
+  turtle.placeDown()
+  turtle.select(1)
+end
 
 function faztunel()
   turtle.dig()
@@ -7,9 +15,18 @@ function faztunel()
   turtle.digDown()
 end
 
+function decideTocha()
+  if contador == 10 then
+    botaTocha()
+    contador=1
+  end
+  contador=contador+1
+end
+
 for num=1,tonumber(total) do
   print("o valor de num e ".. tostring(num))
   faztunel()
+  decideTocha()
 end
 --vire pra direta
 turtle.turnRight()
@@ -20,5 +37,6 @@ end
 turtle.turnRight()
 for num3=1,tonumber(total) do
   faztunel()
+  decideTocha()
 end
 
